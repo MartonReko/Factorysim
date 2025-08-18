c9d6a3f8bdf559200de4fbe357becc0bfcf0d2ae
Petya <vegvaripeter21@gmail.com>
2025-08-18 13:42:31 +0200

    added missing fields

--- jubilant_disco/models.py
+++ jubilant_disco/models.py
@@ -5,19 +5,19 @@ from sqlmodel import SQLModel, Field
 
 class ActorBase(SQLModel):
     id: int | None = Field(default=None, primary_key=True)
-    money: int = 0
+    money: int = Field(default=0)
 
 
 class GoodBase(SQLModel):
     id: int | None = Field(default=None, primary_key=True)
-    name: str
+    name: str = Field()
 
 
 class RecipeItemBase(SQLModel):
     id: int | None = Field(default=None, primary_key=True)
     good_id: int = Field(default=None, foreign_key="good.id")
     recipe_id: int = Field(default=None, foreign_key="recipe.id")
-    quantity: int = 1
+    quantity: int = Field(default=1)
 
     class Type(Enum):
         INPUT = "INPUT"
@@ -28,35 +28,34 @@ class RecipeItemBase(SQLModel):
 
 class RecipeBase(SQLModel):
     id: int | None = Field(default=None, primary_key=True)
-    name: str
+    name: str = Field()
 
 
 class OccupationBase(SQLModel):
     id: int | None = Field(default=None, primary_key=True)
     workplace_id: int | None = Field(default=None, foreign_key="workplace.id")
     person_id: int | None = Field(default=None, foreign_key="person.id")
-    wage: int = 0
-    hours: int = 8
+    wage: int = Field(default=0)
 
 
 class PersonBase(ActorBase):
     id: int | None = Field(default=None, primary_key=True)
     name: str = str(id)
     birthYear: int = 0
-    happiness: int = 0
-    hunger: int = 0
+    happiness: int = Field(default=100)
+    hunger: int = Field(default=0)
 
 
 class WorkplaceBase(ActorBase):
     id: int | None = Field(default=None, primary_key=True)
     recipe_id: int | None = Field(default=None, foreign_key="recipe.id")
-    name: str
-    maxWorkers: int = 0
+    name: str = Field()
+    maxWorkers: int = Field(default=0)
 
 
 class ProductBase(SQLModel):
     id: int | None = Field(default=None, primary_key=True)
     good_id: int | None = Field(default=None, foreign_key="good.id")
     actor_id: int | None = Field(default=None, foreign_key="actor.id")
-    quantity: int = 0
-    price: float | None
+    quantity: int = Field()
+    price: float | None = Field(default=0)
